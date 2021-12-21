7c8aad87995b84f3b2db865f77371635aaa3f5f8
Elle Ti <linh.htu@gmail.com>
2021-12-20 16:41:49 -0800

    Configure babel file

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,19 @@
+const alias = require('./alias');
+
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    'react-native-reanimated/plugin',
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.js', '.ts', '.jsx', '.tsx', '.json'],
+        alias: {
+          tests: ['./tests/'],
+          "@components": "./src/components",
+        },
+      },
+    ],
+  ],
 };
